eea91b556db231d2594fb8b74aba3bb0c7be5813
Vorster Naude <26192349@student.g.nwu.ac.za>
2018-05-16 02:25:30 +0200

    comments added to transposition

--- Cryptography/Cryptography/CryptoClasses/TranspositionClass.cs
+++ Cryptography/Cryptography/CryptoClasses/TranspositionClass.cs
@@ -12,7 +12,7 @@ namespace Cryptography
 
         public static string encrypt(string plaintext, string key)
         {
-            //make all letters uppercase
+            //make all letters uppercase and remove spaces
             plaintext = plaintext.ToUpper();
             plaintext = plaintext.Replace(" ", "");
 
@@ -21,9 +21,13 @@ namespace Cryptography
             Array.Sort(brokenKey);
             string sortedKey = new string(brokenKey);
             
+            //Calculate the number of rows needed in the matrix
             int rows = (int)Math.Ceiling((double)plaintext.Length / key.Length);
+
+            //create the matrix
             string[,] plainTextMatrix = new string[rows, key.Length];
             int ptIndex = 0;
+            //Fill matrix with data and padding
             for(int row = 0; row<rows; row++)
             {
                 for(int col = 0; col<key.Length; col++)
@@ -37,9 +41,8 @@ namespace Cryptography
                     }
                 }
             }
-            Console.WriteLine("Plaintext");
-            printMatrix(plainTextMatrix);
-
+            
+            //Map sorted key indices to original key indices
             int[] alphaToKeyMap = new int[key.Length];
             char[] tempKey = key.ToCharArray();
             for(int i =0; i<alphaToKeyMap.Length; i++)
@@ -58,18 +61,15 @@ namespace Cryptography
             }
                       
             string cipherText = "";
-
+            //Fill cipherText with columns selected according to previous mapping
             for (int col = 0; col < key.Length; col++)
             {
                 for (int row = 0; row < rows; row++)
                 {
-                
                     cipherText += plainTextMatrix[row, alphaToKeyMap[col]];
                 }
             }
-            Console.WriteLine("Ciphertext: " + cipherText);
             return cipherText;
-            
         }
 
         public static string decrypt(string ciphertext, string key)
@@ -78,25 +78,25 @@ namespace Cryptography
             char[] brokenKey = key.ToArray();
             Array.Sort(brokenKey);
             string sortedKey = new string(brokenKey);
-            Console.WriteLine(sortedKey);
+            
 
             //get amount of rows the plain text matrix has
             int rows = (int) Math.Ceiling((double)ciphertext.Length / key.Length);
 
-            string[,] plainTextMatrix = new string[rows, key.Length];
+            string[,] cipherTextMatrix = new string[rows, key.Length];
 
             //reconstruct the key-sorted plaintext matrix
             int index = 0;
-            for(int col = 0; col<key.Length; col++)
+            //Fill matrix with encrypted data
+            for (int col = 0; col<key.Length; col++)
             {
                 for(int row = 0; row<rows; row++)
                 {
-                    plainTextMatrix[row, col] = Convert.ToString(ciphertext[index++]);
+                    cipherTextMatrix[row, col] = Convert.ToString(ciphertext[index++]);
                 }
             }
-            Console.WriteLine("Imported Ciphertext");
-            printMatrix(plainTextMatrix);
 
+            //Map sorted key indices to original key indices
             int[] alphaToKeyMap = new int[key.Length];
             char[] tempKey = sortedKey.ToCharArray();
             for (int i = 0; i < alphaToKeyMap.Length; i++)
@@ -114,15 +114,16 @@ namespace Cryptography
                 alphaToKeyMap[i] = index3;
             }
 
+            //fill output variable with decrypted data 
             string plainText = "";
             for(int row = 0; row<rows; row++)
             {
                 for(int col=0; col<key.Length; col++)
                 {
-                    plainText += plainTextMatrix[row, alphaToKeyMap[col]];
+                    plainText += cipherTextMatrix[row, alphaToKeyMap[col]];
                 }
             }
-            
+            //Remove padding added by encryption
             return plainText.Trim(new char[] { '|'});
 
 
@@ -190,7 +191,7 @@ namespace Cryptography
 
             //create output byte array and fill byte array with columns in the order corresponding to
             //the mapping made in the previous block. Thus the first column to be selected is the 
-            //index of the first character (in the sorted list) in the original key
+            //index of the first character (in the sorted key) in the original key
             byte[] cipherText = new byte[plainTextMatrix.LongLength + 1];
             int index2 = 0;
             for (int col = 0; col < key.Length; col++)
@@ -212,29 +213,34 @@ namespace Cryptography
 
         public static bool decryptFile(string fileName, string key)
         {
+            //Read bytes from plaintext file
             byte[] cipherText = File.ReadAllBytes(fileName);
             byte paddingStartIndex = cipherText[cipherText.Length - 1];
-            Console.WriteLine(paddingStartIndex);
+           
+            
             //Sort key in alphabetical order
             char[] brokenKey = key.ToArray();
             Array.Sort(brokenKey);
             string sortedKey = new string(brokenKey);
 
-            //get amount of rows the plain text matrix has
+            //calculate number of rows each column in the matrix will have
             int rows = (int)Math.Floor((double)(cipherText.Length) / key.Length);
+            //create matrix object
+            byte[,] cipherTextMatrix = new byte[rows, key.Length];
 
-            byte[,] plainTextMatrix = new byte[rows, key.Length];
-
-            //reconstruct the key-sorted plaintext matrix
+            //reconstruct the key-sorted ciphertext matrix
             int index = 0;
             for (int col = 0; col < key.Length; col++)
             {
                 for (int row = 0; row < rows; row++)
                 {
-                    plainTextMatrix[row, col] = cipherText[index++];
+                    //Fill matrix with file data
+                    cipherTextMatrix[row, col] = cipherText[index++];
                 }
             }
 
+            //create an array that maps the sorted key to the original key. This will be used to 
+            //select matrix columns in the correct order for encryption
             int[] alphaToKeyMap = new int[key.Length];
             char[] tempKey = sortedKey.ToCharArray();
             for (int i = 0; i < alphaToKeyMap.Length; i++)
@@ -252,25 +258,30 @@ namespace Cryptography
                 alphaToKeyMap[i] = index3;
             }
 
-            byte[] plainText = new byte[plainTextMatrix.LongLength];
+            //create output byte array and fill byte array with columns in the order corresponding to
+            //the mapping made in the previous block. Thus the first column to be selected is the 
+            //index of the first character (in the original key) in the sorted key
+            byte[] plainText = new byte[cipherTextMatrix.LongLength];
             int index2 = 0;
             for (int row = 0; row < rows; row++)
             {
                 for (int col = 0; col < key.Length; col++)
                 {
-                    plainText[index2++] = plainTextMatrix[row, alphaToKeyMap[col]];
+                    plainText[index2++] = cipherTextMatrix[row, alphaToKeyMap[col]];
                 }
             }
                         
             //remove paddings bytes
             Array.Resize(ref plainText, plainText.Length-(key.Length - paddingStartIndex));
+
             fileName = fileName.Replace(".transposition", "");
             string fileExtension = fileName.Substring(fileName.LastIndexOf('.'));
+            //Write the byte array to disk
             ByteArrayToFile(fileName+".decrypted"+fileExtension, plainText);
             return true;
         }
 
-
+        //Method for writing byte array to file using filestream
         public static bool ByteArrayToFile(string fileName, byte[] byteArray)
         {
             try
@@ -288,29 +299,9 @@ namespace Cryptography
             }
         }
 
-        public static void printMatrix(string[,] matrix)
-        {
-            for(int row =0; row<matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    Console.Write(matrix[row, col] + "\t");
-                }
-                Console.WriteLine("\r\n");
-            }
-        }
+        
 
-        public static void printMatrix(byte[,] matrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
-            {
-                for (int col = 0; col < matrix.GetLength(1); col++)
-                {
-                    Console.Write(matrix[row, col] + "\t");
-                }
-                Console.WriteLine("\r\n");
-            }
-        }
+        
 
 
     }
